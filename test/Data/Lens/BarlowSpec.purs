6b843a7ccfbd904b1c6ab0fc06803094dff1f1ad
sigma-andex <sigma.andex@pm.me>
2021-03-21 09:46:27 +0000

    Add support for either and traversables
    - Add symbol > for Right lens and < for Left lens
    - Add symbol + for traversable like Array

--- test/Data/Lens/BarlowSpec.purs
+++ test/Data/Lens/BarlowSpec.purs
@@ -1,5 +1,6 @@
 module Data.Lens.BarloSpec where
 
+import Data.Either (Either(..))
 import Data.Lens (over, preview, view)
 import Data.Lens.Barlow (barlow, key)
 import Data.Maybe (Maybe(..))
@@ -132,3 +133,110 @@ spec =
 
           actual = preview (barlow (key :: _ "zodiac?.virgo?")) sky
         actual `shouldEqual` (Just expected)
+      it "should view into a record with Either" do
+        let
+          sky =
+            { zodiac:
+                Right
+                  { virgo:
+                      { alpha: "Spica"
+                      }
+                  }
+            }
+
+          expected =
+            { virgo:
+                { alpha: "Spica"
+                }
+            }
+
+          actual = preview (barlow (key :: _ "zodiac>")) sky
+        actual `shouldEqual` (Just expected)
+      it "should view into a record with Either (2)" do
+        let
+          sky =
+            { zodiac:
+                Right
+                  { virgo:
+                      Right
+                        { alpha: Right "Spica"
+                        }
+                  }
+            }
+
+          actual = preview (barlow (key :: _ "zodiac>.virgo>.alpha>")) sky
+        actual `shouldEqual` (Just "Spica")
+      it "should view into a record with mixed Just and Either" do
+        let
+          sky =
+            { zodiac:
+                Right
+                  { virgo:
+                      Just
+                        { alpha: Left "Spica"
+                        }
+                  }
+            }
+
+          actual = preview (barlow (key :: _ "zodiac>.virgo?.alpha<")) sky
+        actual `shouldEqual` (Just "Spica")
+      it "should modify a record with mixed Just and Either" do
+        let
+          -- type annotation necessary in this test for Show 
+          sky :: { zodiac :: Either String { virgo :: Maybe { alpha :: Either String Int } } }
+          sky =
+            { zodiac:
+                Right
+                  { virgo:
+                      Just
+                        { alpha: Left "Spica"
+                        }
+                  }
+            }
+
+          expected =
+            { zodiac:
+                Right
+                  { virgo:
+                      Just
+                        { alpha: Left "SPICA"
+                        }
+                  }
+            }
+
+          actual = over (barlow (key :: _ "zodiac>.virgo?.alpha<")) toUpper sky
+        actual `shouldEqual` expected
+      it "should modify a record with Array" do
+        let
+          sky =
+            { zodiac:
+                [ { virgo:
+                      Just
+                        { star: "Spica"
+                        }
+                  }
+                , { virgo:
+                      Just
+                        { star: "Serpentis"
+                        }
+                  }
+                ]
+            }
+
+          expected =
+            { zodiac:
+                [ { virgo:
+                      Just
+                        { star: "SPICA"
+                        }
+                  }
+                , { virgo:
+                      Just
+                        { star: "SERPENTIS"
+                        }
+                  }
+                ]
+            }
+
+          actual = over (barlow (key :: _ "zodiac+.virgo?.star")) toUpper sky
+        actual `shouldEqual` expected
