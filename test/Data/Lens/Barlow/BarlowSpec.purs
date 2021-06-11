139c929c11762bc33778fa6675ea5ecfc3b986c6
sigma-andex <sigma.andex@pm.me>
2021-06-11 19:00:27 +0100

    Cleanup

--- test/Data/Lens/Barlow/BarlowSpec.purs
+++ test/Data/Lens/Barlow/BarlowSpec.purs
@@ -310,7 +310,6 @@ spec =
         actual `shouldEqual` expected
       it "should view into a record with Newtype" do
         let
-          sky :: { zodiac :: Maybe { virgo :: AlphaR }}
           sky =
             { zodiac:
                 Just
